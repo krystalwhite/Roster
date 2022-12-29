831676020ac9fec26e88746b8ba661f0b5e4c40f
krystalwhite <krystalsnowhite@gmail.com>
2022-12-29 16:28:17 -0600

    copying basic template framework into other files

--- Roster/src/main/java/com/krystalwhite/Roster/controllers/PlayerController.java
+++ Roster/src/main/java/com/krystalwhite/Roster/controllers/PlayerController.java
@@ -1,15 +1,9 @@
 package com.krystalwhite.Roster.controllers;
 
 import com.krystalwhite.Roster.models.Player;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
-
 @Controller
 @RequestMapping("player")
 public class PlayerController {
