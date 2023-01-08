a046b425c8f52ab6524cd5bff8b1b453d38400d6
krystalwhite <krystalsnowhite@gmail.com>
2023-01-07 19:06:40 -0600

    front and back end of barebones Coach class

--- Roster/src/main/java/com/krystalwhite/Roster/models/Coach.java
+++ Roster/src/main/java/com/krystalwhite/Roster/models/Coach.java
@@ -4,81 +4,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
-import java.util.List;
-import java.util.Objects;
-
-//@Entity
+@Entity @Data
 public class Coach {
 
-//    @Id @GeneratedValue
+    @Id @GeneratedValue
     private int id;
 
-    @NotBlank(message="Name is required")
-    private String name;
+    @NotBlank(message = "First name is required.")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required.")
+    private String lastName;
 
     @NotBlank(message="Sport is required")
     private String sport;
-    private List<Player> players;
+
     public Coach() {}
-    public Coach(String name, String sport) {
-        this.name=name;
+    public Coach(String firstName, String lastName, String sport) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.sport=sport;
-    }
-    public Coach(String name, String sport, List<Player> players){
-        this.name = name;
-        this.sport = sport;
-        this.players = players;
-    }
-    @Override
-    public String toString() {
-        return "Coach{" +
-                "name='" + name + '\'' +
-                ", sport='" + sport + '\'' +
-                ", players=" + players +
-                '}';
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Coach coach = (Coach) o;
-        return id == coach.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSport() {
-        return sport;
-    }
-
-    public void setSport(String sport) {
-        this.sport = sport;
-    }
-
-    public List<Player> getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(List<Player> players) {
-        this.players = players;
     }
 }
